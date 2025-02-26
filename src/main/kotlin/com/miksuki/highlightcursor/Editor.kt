cd5f95688f2b5d68632d3a06ff1f98e17b4fed15
miksuki <miksuki.hu@gmail.com>
2025-02-27 01:10:42 +0800

    can get current vim mode

--- src/main/kotlin/com/miksuki/highlightcursor/Editor.kt
+++ src/main/kotlin/com/miksuki/highlightcursor/Editor.kt
@@ -3,6 +3,7 @@ package com.miksuki.highlightcursor
 import com.intellij.openapi.editor.colors.EditorColors.*
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
+import com.maddyhome.idea.vim.VimPlugin.getEditor
 import java.awt.Color
 
 class Editor : EditorFactoryListener {
@@ -10,5 +11,9 @@ class Editor : EditorFactoryListener {
         super.editorCreated(event)
         val editor = event.editor
         editor.colorsScheme.setColor(CARET_COLOR, Color.decode(AppSettings.getInstance().state?.color))
+        val vimEditor = getEditor()
+        vimEditor.getEditors().map { e ->
+            println("current vim mode is: ${e.mode}")
+        }
     }
 }
